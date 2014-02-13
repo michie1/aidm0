7a60c7ebc6f4fee68ec64acf59949e5b200f1c0b
Michiel <git@fastfox.nl>
2014-02-13 18:44:47 +0100

    task3

--- task3.py
+++ task3.py
@@ -1,6 +1,13 @@
 from scipy import stats
-K = 1
-N = 10
-L = 5
-print stats.binom.pmf(L, N, 1/N)
+#K = 1  
+#N = 10
+#L = 5
 
+k = 2 # number of heads
+n = 2 # number of coin tosses
+p = 0.5 # probability of heads
+
+print stats.binom.pmf(k, n, p) # 0.25
+
+
+# 'k snap niet hoe je hier zo'n vector uit krijgt.
