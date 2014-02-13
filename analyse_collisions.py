f399c9c88e8ace818ec0c7e6931860294dd1e427
Michiel <git@fastfox.nl>
2014-02-13 14:11:34 +0100

    Task1

--- analyse_collisions.py
+++ analyse_collisions.py
@@ -1,9 +1,19 @@
 from numpy import *
 
-def analyse_collisions():
-	K = 100 # Objects
-	N = 1000 # Buckets
-	L = 5 # ?
-	rand_int = random.randint(1, N + 1, K)
-	print 'hoi'
-	print size(rand_int)
+def analyse_collisions(K, N, L):
+	# K = 100 # # Objects
+	# N = 1000 # # Buckets
+	# L = 5 # Count size
+	buckets = [0] * N # # of objects in bucket
+	counts = [0] * (L + 1) # # of buckets with l objects
+	rand_int = random.randint(1, N + 1, K) # Random objects
+	
+	# Count elements in buckets
+	for i in rand_int:
+		buckets[i] += 1
+
+	# Count buckets with specific amount of objects in bucket
+	for n in range(N):
+		counts[buckets[n]] += 1
+
+	return counts 
--- task1.py
+++ task1.py
@@ -1,2 +1,2 @@
 from analyse_collisions import analyse_collisions
-analyse_collisions()
+print analyse_collisions(100, 1000, 5)
