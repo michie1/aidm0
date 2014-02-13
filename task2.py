fe33a0618cb394e1875131873ebd4d0d5ca65042
Michiel <git@fastfox.nl>
2014-02-13 21:23:52 +0100

    Fix integer -> float

--- task2.py
+++ task2.py
@@ -2,7 +2,7 @@ from numpy import *
 K = 100
 N = 1000
 L = 5
-buckets = random.binomial(K, 1/N, N)
+buckets = random.binomial(K, 1.0/N, N)
 counts = [0] * (L + 1) # # of buckets with l objects
 
 # Count buckets with specific amount of objects in bucket
