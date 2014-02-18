fc4ed41b2baf9ad8176cf5da301a285295d8f789
Michiel <msvos@liacs.nl>
2014-02-18 11:16:51 +0100

    task3 aangepast met Mathe

--- task3.py
+++ task3.py
@@ -1,13 +1,18 @@
 from scipy import stats
-#K = 1  
+#K = 1 
 #N = 10
 #L = 5
 
-k = 2 # number of heads
-n = 2 # number of coin tosses
-p = 0.5 # probability of heads
+#k = 2 # number of heads
+#n = 2 # number of coin tosses
+#p = 0.5 # probability of heads
+k = 1
+n = 10
+l = 5
+counts = [0] * l
 
-print stats.binom.pmf(k, n, p) # 0.25
+for i in range(l):
+	counts[i] = stats.binom.pmf(k, n, i) * n
+	print stats.binom.pmf(k, n, i)
 
-
-# 'k snap niet hoe je hier zo'n vector uit krijgt.
+print counts
